39b3fe9593502ae7fb0058f7e31211cba712f19e
MenakaMaggi <47637535+MenakaMaggi@users.noreply.github.com>
2019-08-12 22:14:20 +0530

    Create set1.9.c

---
+++ set1.9.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+   int a[5],i,j,k,n,sum=0;
+   printf("Enter the N value:");
+   scanf("%d%d",&a[n],&k);
+   for(i=0;i<=n;i++)
+   {
+       printf("%d\n",n);
+   }
+   for(j=0;j<=k;j++)
+   {
+   sum=sum+j;
+   }
+printf("sum value:%d",sum);
+
+    return 0;
+}
