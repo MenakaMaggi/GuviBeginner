11591578dfbdb4af29fea1abf3c39b41aaf56ee3
MenakaMaggi <47637535+MenakaMaggi@users.noreply.github.com>
2019-08-13 20:32:38 +0530

    Create set2.2.c

---
+++ set2.2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,rev=0,temp;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    temp=n;
+    while(temp!=0)
+    {
+        rev=rev*10;
+        rev=rev+temp%10;
+        temp=temp/10;
+    }
+    if(n==rev)
+        printf("yes");
+    else
+        printf("no");
+}
