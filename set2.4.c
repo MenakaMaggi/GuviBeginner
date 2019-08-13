038329a523669e53c0db98471604b754f14b1385
MenakaMaggi <47637535+MenakaMaggi@users.noreply.github.com>
2019-08-13 21:05:54 +0530

    Create set2.4.c

---
+++ set2.4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int num,m,n;
+    printf("Enter the value of m:");
+    scanf("%d",&m);
+    printf("Enter the value of n:");
+    scanf("%d",&n);
+    for(num=m;num<=n;num++)
+    {
+        if(num%2==1)
+        {
+            printf("\t%d",num);
+        }
+      
+    }
+}
